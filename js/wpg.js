d0eca65a39add86d7b623a5d30b1d69d043bcd9c
Edward Ellsworth <eellsworth@mediasolutionscorp.com>
2014-08-22 15:12:22 -0700

    initial js hook system

--- js/wpg.js
+++ js/wpg.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 	
 	var wp_geek = {
 		popupwidth: function(){
-				return $(window).width() * .7;
+				return wpg_hook.filter('popupwidth', $(window).width() * .7);
 			},//popupwidth
 			
 		popupheight: function(){
@@ -10,6 +10,7 @@ jQuery(document).ready(function($){
 			},//popupheight
 		
 		init:function(){
+
 			$('.wpg_lightbox').each(function(){
 				$(this).colorbox();	
 			});//.each
@@ -27,7 +28,67 @@ jQuery(document).ready(function($){
 	}//wp_geek
 	
 	wp_geek.init();
+
+	//hooking system
+	var wpg_hook = {
+		hooks:{},
+
+		register:{
+			filter:function(hook, callback){
+				if(!wpg_hook.hooks[hook]){ wpg_hook.hooks[hook] = [];}
+
+				wpg_hook.hooks[hook].push(callback);
+			},
+			action:function(hook, callback){
+				if(!wpg_hook.hooks[hook]){ wpg_hook.hooks[hook] = [];}
+
+				wpg_hook.hooks[hook].push(callback);
+			}
+		},//register
+
+		filter:function(hook, val){
+
+			if(!wpg_hook.hooks[hook]){return val;}
+
+			var retval = val;
+			var hooks = wpg_hook.hooks[hook];
+
+			var l = hooks.length;
+
+			for (var i=0;i<l; i++) {
+				var callback = hooks[i];
+				retval = callback(retval);
+			}
+
+			return retval;
+		},//filter
+		
+		action:function(hook, args){
+			if(!wpg_hook.hooks[hook]){return false;}
+
+			var hooks = wpg_hook.hooks[hook];
+			var l = hooks.length;
+
+			for (var i=0;i<l; i++) {
+				var callback = hooks[i];
+				callback(args);
+			}
+
+
+		}//action
+
+	};//wpg_hooks	
+/*
+	wpg_hook.register.filter('popupwidth', function(val){
+		return 'test ' + val;
+	});
+
+	wpg_hook.register.filter('minor', function(val){
+			return parseInt(val, 16)
+	});
+
+
+	console.log(wpg_hook.filter('popupwidth', 'filtered value'));
 	
-
-
+	console.log(wpg_hook.filter('minor', '29'));*/
 });//doc ready
